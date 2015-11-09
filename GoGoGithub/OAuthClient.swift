82f3a3363bafde899eb91e585bde4a25e57068b8
Alberto-Vega <el_vega@live.com>
2015-11-09 15:25:02 -0800

    Initial Commit

---
+++ GoGoGithub/OAuthClient.swift
@@ -0,0 +1,9 @@
+//
+//  OAuthClient.swift
+//  GoGoGithub
+//
+//  Created by Alberto Vega Gonzalez on 11/9/15.
+//  Copyright © 2015 Alberto Vega Gonzalez. All rights reserved.
+//
+
+import Foundation
