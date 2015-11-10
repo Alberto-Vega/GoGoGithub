f9c1b50ce80b027f3ad164ada25129a48a98d3c3
Alberto-Vega <el_vega@live.com>
2015-11-09 17:00:07 -0800

    Initial Commit

--- GoGoGithub/OAuthClient.swift
+++ GoGoGithub/OAuthClient.swift
@@ -6,4 +6,67 @@
 //  Copyright © 2015 Alberto Vega Gonzalez. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+let kAccessTokenKey = "kAccessTokenKey"
+let kOAuthBaseURLString = "https://github.com/login/oauth/"
+let kAccessTokenRegexPattern = "access_token=([^&]+)"  //this is incomplete you can
+//use different things to find a part of a string.
+let kClientId = "405cf0ef46239dcd8971"
+let kClientSecret = "b4418a2aab61ccf560cff2ab917e53eb9e19bed1"
+
+typealias OAuthClientCompletion = (sucess: Bool) -> ()
+
+enum OAuthClientError: ErrorType {
+    case MissingAccessToken(String)
+    case ExtractingTokenFromString()
+    case ExtractingTemporaryCode(String)
+    case ResponseFromGithub(String)
+}
+
+enum SaveOptions: Int {
+    case UserDefaults
+}
+
+class OAuthClient {
+    
+    
+        
+        static let shared = OAuthClient()
+    
+    struct scope {
+        var user = "user"
+        var email = "email"
+        var repo = "repo"
+    }
+    
+    func requestGithubAccess() {
+        let authURL = NSURL(string: "\(kOAuthBaseURLString)authorize?client_id=\(kClientId)&scope=scope,email,repo)")
+    }
+        
+        func oauthRequestWithParameters(parameters: [String : String]) {
+            var parameterString = String()
+            for parameter in parameters.values {
+                parameterString.stringByAppendingString(parameter)
+            }
+    
+            // URL Constructor.
+            func exchangeCodeInURL(codeURL : NSURL) {
+                if let code = codeURL.query {
+                    let request = NSMutableURLRequest(URL: NSURL(string: "https://github.com/login/oauth/access_token?\(code)&client_id=\(kClientId)&client_secret=\(kClientSecret)")!)
+                    request.HTTPMethod = "POST"
+                    request.setValue("application/json", forHTTPHeaderField: "Accept")
+                    NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: { (data, response, error) -> Void in
+                        if let httpResponse = response as? NSHTTPURLResponse {
+                            var jsonError: NSError?
+                            if let rootObject = NSJSONSerialization.JSONObjectWithData(data!, options: nil) as? [String : AnyObject], token = rootObject["access_token"] as? String {
+                                //Save and handle the token
+                            }
+                        }
+                    }).resume()
+                }
+            }
+
+}
+}
+   
--- GoGoGithub/ViewController.swift
+++ GoGoGithub/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        OAuthClient.requestGithubAccess(
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func loginButtonSelected(sender: UIButton) {
+//        OAuthClient.shared.
+    }
 
 }
 
